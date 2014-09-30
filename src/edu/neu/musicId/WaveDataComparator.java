b752565fb4c4a9d6cb57d25accdca3a05f8b96d1
ckolek <ckolek@gmail.com>
2014-09-30 19:50:19 -0400

    stub out WaveDataComparator class

---
+++ src/edu/neu/musicId/WaveDataComparator.java
@@ -0,0 +1,9 @@
+package edu.neu.musicId;
+
+import edu.neu.musicId.wav.WaveData;
+
+public class WaveDataComparator {
+    public boolean areMatching(WaveData wave1, WaveData wave2) {
+        return false;
+    }
+}
