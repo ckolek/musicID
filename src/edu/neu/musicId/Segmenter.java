42543982faf0bd1f0e880b8d5b2e097d489669f3
ckolek <ckolek@gmail.com>
2014-10-01 18:54:16 -0400

    Update getSamples() to return array of average value of samples from all channels

--- src/edu/neu/musicId/Segmenter.java
+++ src/edu/neu/musicId/Segmenter.java
@@ -1,3 +1,4 @@
+
 package edu.neu.musicId;
 
 import java.util.ArrayList;
@@ -9,12 +10,12 @@ import edu.neu.musicId.wav.WaveData.Chunk;
 import edu.neu.musicId.wav.WaveDataFormat;
 
 public class Segmenter {
-    public float[][] segmentData(WaveData wave){
+    public float[][] segmentData(WaveData wave) {
         WaveDataFormat waveDataFormat1 = wave.getWaveDataFormat();
 
         Chunk chunk = wave.getChunk("data");
 
-        float[][] data = getSamples(wave, waveDataFormat1, chunk);
+        float[] data = getSamples(wave, waveDataFormat1, chunk);
 
         // Computes the size of a float in bytes (answer: 4)
         final int FLOAT_SIZE = Float.SIZE / Byte.SIZE;
@@ -24,10 +25,11 @@ public class Segmenter {
 
         // Compute the number of samples in each segment interval by:
         // segment length * byterate / (number of bytes per sample)
-        int segmentInterval_numSamples = (int)(segmentInterval * waveDataFormat1.getByteRate()) / waveDataFormat1.getBytesPerSample();
+        int segmentInterval_numSamples = (int) (segmentInterval * waveDataFormat1.getByteRate())
+                / waveDataFormat1.getBytesPerSample();
 
         // Compute the number of segments in this wave
-        int numSegments = (int)(segmentInterval_numSamples / waveDataFormat1.getTime(chunk));
+        int numSegments = (int) (segmentInterval_numSamples / waveDataFormat1.getTime(chunk));
 
         // Initialize array of arrays with dimensions:
         // Num Segments long x Num Samples per Segment wide
@@ -45,7 +47,7 @@ public class Segmenter {
         // cnt counter, and increment the arrayIndex counter.
         for (float _float : data[arrayIndex]) {
             // If the counter has reached the segment interval
-            if(cnt > segmentInterval_numSamples) {
+            if (cnt > segmentInterval_numSamples) {
                 // Save the temp array of floats to segmentedData
                 segmentedData[arrayIndex] = tempArray_floats;
 
@@ -66,35 +68,29 @@ public class Segmenter {
 
         return segmentedData;
     }
-    
+
     private static float getSegmentInterval(WaveDataFormat format, Chunk chunk) {
         return 1F;
     }
-    
-    private static float[][] getSamples(WaveData wave, WaveDataFormat format, Chunk chunk) {
+
+    private static float[] getSamples(WaveData wave, WaveDataFormat format, Chunk chunk) {
         byte[][] channels = chunk.extractChannels(format);
 
         final int bytesPerSample = format.getBytesPerSample();
+        final int blockAlign = format.getBlockAlign();
+        final int numSamples = chunk.length() / blockAlign;
 
-        float[][] samples = new float[channels.length][];
+        float[] samples = new float[numSamples];
 
         for (int i = 0; i < samples.length; i++) {
-            byte[] channel = channels[i];
+            float sample = 0;
 
-            final int numSamples = channel.length / bytesPerSample;
-
-            float[] channelSamples = new float[numSamples];
-
-            for (int j = 0; j < numSamples; j += bytesPerSample) {
-                float value = Utilities.toInt(channel, j * bytesPerSample, bytesPerSample,
-                        (bytesPerSample < 2), wave.isLittleEndian());
-
-                // TODO: divide value by maximum sample value
-
-                channelSamples[j] = value;
+            for (int j = 0; j < channels.length; j++) {
+                sample += Utilities.toInt(channels[j], (i * blockAlign) + (j * bytesPerSample),
+                        bytesPerSample, (bytesPerSample < 2), wave.isLittleEndian());
             }
 
-            samples[i] = channelSamples;
+            samples[i] = sample / channels.length;
         }
 
         return samples;
