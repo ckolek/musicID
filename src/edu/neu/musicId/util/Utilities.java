1d4c212619ccdcc25e0825e9b5642788838bf3e0
ckolek <ckolek@gmail.com>
2014-09-30 20:34:06 -0400

    use constants for clarity

--- src/edu/neu/musicId/util/Utilities.java
+++ src/edu/neu/musicId/util/Utilities.java
@@ -7,7 +7,7 @@ public final class Utilities {
     }
 
     public static int toInt16(byte[] bytes, int offset, boolean isLittleEndian) {
-        return (int) toInt(bytes, offset, 2, isLittleEndian);
+        return (int) toInt(bytes, offset, Short.SIZE / Byte.SIZE, isLittleEndian);
     }
 
     public static int toInt16(byte[] bytes, boolean isLittleEndian) {
@@ -15,7 +15,7 @@ public final class Utilities {
     }
 
     public static long toInt32(byte[] bytes, int offset, boolean isLittleEndian) {
-        return toInt(bytes, offset, 4, isLittleEndian);
+        return toInt(bytes, offset, Integer.SIZE / Byte.SIZE, isLittleEndian);
     }
 
     public static long toInt32(byte[] bytes, boolean isLittleEndian) {
@@ -27,17 +27,18 @@ public final class Utilities {
 
         for (int i = 0; i < length; i++) {
             byte b = isLittleEndian ? bytes[offset + i] : bytes[offset + (length - 1 - i)];
-            
-            value += (0xFF & b) << (8 * i);
+
+            value += (0xFF & b) << (Byte.SIZE * i);
         }
 
         return value;
     }
-    
+
     public static float toFloat(byte[] bytes, int offset, boolean isLittleEndian) {
-        return Float.intBitsToFloat((int) toInt(bytes, offset, 4, isLittleEndian));
+        return Float.intBitsToFloat((int) toInt(bytes, offset, Float.SIZE / Byte.SIZE,
+                isLittleEndian));
     }
-    
+
     public static float toFloat(byte[] bytes, boolean isLittleEndian) {
         return toFloat(bytes, 0, isLittleEndian);
     }
