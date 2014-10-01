36fb5ea3533f46e65ca9828ad020d0d0eb0fd8e6
ckolek <ckolek@gmail.com>
2014-09-30 21:00:09 -0400

    Add small code changes

--- src/edu/neu/musicId/Segmenter.java
+++ src/edu/neu/musicId/Segmenter.java
@@ -1,34 +1,40 @@
 package edu.neu.musicId;
 
+import edu.neu.musicId.util.Utilities;
 import edu.neu.musicId.wav.WaveData;
 import edu.neu.musicId.wav.WaveData.Chunk;
 import edu.neu.musicId.wav.WaveDataFormat;
-import edu.neu.musicId.util.Utilities;
-
-import java.util.Arrays;
 
 /**
  * Created by Deniz on 9/30/2014.
  */
 public class Segmenter {
-    public float[][] segmentFile(WaveData wave, float segmentInterval){
-        float[][] segmentedFile = new float[][]{};
-
+    public float[][] segmentData(WaveData wave){
         WaveDataFormat waveDataFormat1 = WaveDataFormat.fromWaveData(wave);
 
         Chunk chunk = wave.getChunk("data");
 
         byte[] data = chunk.getData();
 
+        final int FLOAT_SIZE = Float.SIZE / Byte.SIZE;
+        
+        float[] floats = new float[data.length / FLOAT_SIZE];
         // Populate the array of arrays of floats by calling the toFloat Utilities method
         // and incrementing the counter by 4
-        for (int i = 0; i < data.length; i += 4) {
-            Arrays.fill(segmentedFile, Utilities.toFloat(data, i, wave.isLittleEndian()));
+        for (int i = 0; i < floats.length; i++) {
+            floats[i] = Utilities.toFloat(data, FLOAT_SIZE * i, wave.isLittleEndian());
         }
+        
+        final float segmentInterval = getSegmentInterval(waveDataFormat1, chunk);
+        
+        float[][] segmentedData = new float[][]{};
 
-        System.out.println(segmentedFile);
+        System.out.println(segmentedData);
 
-        return segmentedFile;
+        return segmentedData;
     }
-
+    
+    private static float getSegmentInterval(WaveDataFormat format, Chunk chunk) {
+        return 1F;
+    }
 }
--- src/edu/neu/musicId/WaveDataComparator.java
+++ src/edu/neu/musicId/WaveDataComparator.java
@@ -1,3 +1,4 @@
+
 package edu.neu.musicId;
 
 import edu.neu.musicId.wav.WaveData;
@@ -11,20 +12,20 @@ public class WaveDataComparator {
         WaveDataFormat waveDataFormat2 = WaveDataFormat.fromWaveData(wave2);
 
         Chunk chunk1 = wave1.getChunk("data");
-        Chunk chunk2 = wave1.getChunk("data");
-        if(waveDataFormat1.getTime(chunk1) != waveDataFormat2.getTime(chunk2)) {
+        Chunk chunk2 = wave2.getChunk("data");
+        
+        if (waveDataFormat1.getTime(chunk1) != waveDataFormat2.getTime(chunk2)) {
             return false;
-        }else {
-            // Calculate the segment interval TODO: Chris' work here
-            float segmentInterval = 1.0f;
-
-            // Segment both files TODO: Deniz's work here
-            Segmenter segmenter = new Segmenter();
-            segmenter.segmentFile(wave1, segmentInterval);
-
-            // Do actual comparison TODO: Jeff's work here
-
-            return true;
         }
+        
+        // Segment both files TODO: Deniz's work here
+        Segmenter segmenter = new Segmenter();
+
+        float[][] wave1segments = segmenter.segmentData(wave1);
+        float[][] wave2segments = segmenter.segmentData(wave2);
+
+        // Do actual comparison TODO: Jeff's work here
+
+        return true;
     }
 }
