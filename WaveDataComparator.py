b45dca5cd461ed2877f42b395c23758134b4d325
ckolek <ckolek@gmail.com>
2014-10-03 15:12:53 -0400

    Simplify segmentation of samples

--- WaveDataComparator.py
+++ WaveDataComparator.py
@@ -1,25 +1,22 @@
 __author__ = 'Deniz'
 
-from wave.waveData import WaveData
-from wave.waveDataFormat import WaveDataFormat
+import Segmenter
+
 
 class WaveDataComparator:
     def areMatching(self, wave1, wave2):
-        waveDataFormat1 = wave1.get_wave_data_format()
-        waveDataFormat2 = wave2.get_wave_data_format()
+        waveDataFormat1 = wave1.wave_data_format
+        waveDataFormat2 = wave2.wave_data_format
 
         chunk1 = wave1.chunk("data")
         chunk2 = wave2.chunk("data")
 
         # Check if files are same length in time
-        if(waveDataFormat1.get_Time(chunk1) != waveDataFormat2.get_Time(chunk2)):
-            return false
+        if waveDataFormat1.get_time(chunk1) != waveDataFormat2.get_time(chunk2):
+            return False
 
-        # Segment both files
-        segmenter = Segmenter()
-
-        wave1segments = segmenter.segmentData(wave1)
-        wave2segments = segmenter.segmentData(wave2)
+        wave1segments = Segmenter.segmentData(wave1)
+        wave2segments = Segmenter.segmentData(wave2)
 
         # Do actual comparison of segments here
-        return true
+        return True
