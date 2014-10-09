09ca67b5cb72c03b47fec6052aa31270d974d6e3
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2014-10-09 14:20:51 -0400

    Adding Mike's work
    Mike's work fine tuning the matching criteria

--- WaveDataComparator.py
+++ WaveDataComparator.py
@@ -1,4 +1,4 @@
-__author__ = 'Deniz'
+__author__ = 'Mike_Deniz'
 
 import Segmenter
 from numpy.fft import fft
@@ -23,25 +23,38 @@ class WaveDataComparator:
         wave1segments = Segmenter.segment_data(self.wave1)
         wave2segments = Segmenter.segment_data(self.wave2)
 
+        trace = 0
+
         # comparing each segment of wave1 to all in wave2
         # slower than going through each simultaneously, but will likely
         # be needed for final version
         for x in wave1segments:
             for y in wave2segments:
                 # if the transforms of two segments match, return a match
-                if self.compareTransform(fft(x), fft(y)):
+                if self.compareTransform(fft(x), fft(y), trace):
                     return True
+                trace += 1
 
         # If no segments matched throughout entire files, return False
         return False
 
     # Compares two discrete fourier transforms
-    def compareTransform(self, dft1, dft2):
+    def compareTransform(self, dft1, dft2, x):
 
         # new comparison checks to see that all constituent frequencies
         # gotten from ffts of segments are no more than 1% different
         l1 = list(dft1)
+        #l1.sort()
+        #l1.reverse()
         l2 = list(dft2)
+        #l2.sort()
+        #l2.reverse()
+    
+        #convert lists to their sorted power spectral densities
+        #where only the top 20% most powerful frequencies are present
+        l1 = sorted(map(lambda x: abs(x)**2, l1), reverse=True)[0:int(len(l1)/5)]
+        l2 = sorted(map(lambda x: abs(x)**2, l2), reverse=True)[0:int(len(l2)/5)]
+
 
         # double check to make sure lists are of equal size
         if len(l1) != len(l2):
@@ -51,8 +64,7 @@ class WaveDataComparator:
         val = True
         # iterate through l1 and l2, check to see constituent frequencies are all very close
         for i in range(len(l1)):
-            val = val and (.97 < (l1[i]/l2[i]) < 1.03)
+            val = val and (.9 < (l1[i]/l2[i]) < 1.1)
 
         return val
-
 
