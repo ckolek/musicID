906378915291f10d24323a99b2e333975502d64c
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2014-11-18 22:23:21 -0500

    Deleted unused constants

--- WaveDataComparator.py
+++ WaveDataComparator.py
@@ -26,44 +26,6 @@ Dejavu library
 # the range frequencies we can detect.
 DEFAULT_FS = 44100
 
-# Size of the FFT window, affects frequency granularity
-DEFAULT_WINDOW_SIZE = 4096
-
-# Ratio by which each sequential window overlaps the last and the
-# next window. Higher overlap will allow a higher granularity of offset
-# matching, but potentially more fingerprints.
-DEFAULT_OVERLAP_RATIO = 0.5
-
-# Degree to which a fingerprint can be paired with its neighbours;
-# higher will cause more fingerprints, but potentially better accuracy.
-DEFAULT_FAN_VALUE = 15
-
-# Minimum amplitude in spectrogram in order to be considered a peak.
-# This can be raised to reduce number of fingerprints, but can negatively
-# affect accuracy.
-DEFAULT_AMP_MIN = 10
-
-# Number of cells around an amplitude peak in the spectrogram in order
-# to consider it a spectral peak. Higher values mean less fingerprints and
-# faster matching, but can potentially affect accuracy.
-PEAK_NEIGHBORHOOD_SIZE = 20
-
-# Thresholds on how close or far fingerprints can be in time in order
-# to be paired as a fingerprint. If your max is too low, higher values of
-# DEFAULT_FAN_VALUE may not perform as expected.
-MIN_HASH_TIME_DELTA = 0
-MAX_HASH_TIME_DELTA = 200
-
-# If True, will sort peaks temporally for fingerprinting;
-# not sorting will cut down number of fingerprints, but potentially
-# affect performance.
-PEAK_SORT = True
-
-# Number of bits to throw away from the front of the SHA1 hash in the
-# fingerprint calculation. The more you throw away, the less storage, but
-# potentially higher collisions and misclassification's when identifying songs.
-FINGERPRINT_REDUCTION = 20
-
 # samples in a given segment
 SAMPLES_PER_SEGMENT = Segmenter.SAMPLES_PER_SEGMENT
 
